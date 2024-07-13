94e7137407883a35f99521071f9d736b192b5ed8
The Algoslingers <58689583+TheAlgoslingers@users.noreply.github.com>
2024-07-13 16:29:23 +0000

    Add files via upload

--- src/examples/test.php
+++ src/examples/test.php
@@ -1,1 +1,17 @@
+<?php
+require 'vendor/autoload.php';
 
+use thealgoslingers\AutoPrice;
+
+// Usage example
+$api_key = 'your_api_key_here'; // api key from ip2location.io
+
+// the default base price is approximately
+// or same as 1 USD. So we assume price is 
+// being converted from USD 
+$base_price = 100.0;//base price to be converted from
+
+$dp = new AutoPrice($api_key, $base_price);
+$user_ip = '8.8.8.8';// ip of the user 
+
+echo $dp->calculatePrice($user_ip);
